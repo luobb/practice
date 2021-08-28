75f0c7f4fad4883877768d33e125d9340429d9a7
luobb <1358375600@qq.com>
2021-08-29 00:42:11 +0800

    test

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
+        String[] strs = {"aa", "bb", "cc"};
+        List<String> stringList = Arrays.asList(strs);
+        Iterator<String> iterator = stringList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
         System.out.println("hahaha");
     }
 }
